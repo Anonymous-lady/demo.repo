752f5602334f6779b208ae3c0afd981060fe345a
Esther Ololade Akinpelu <123809279+Anonymous-lady@users.noreply.github.com>
2024-07-16 21:23:21 -0600

    Update cash.py
    Helps to calculate  the number of quarters, dimes, nickels and pennies a cashier owes a customer.

--- cash.py
+++ cash.py
@@ -1,1 +1,30 @@
-c
+def get_cents():
+  while True:
+    cents = int(input("Cash Owed: "))
+    if cents >= 0:
+      return cents
+    
+def cal_coins(cents):
+  quarters = cents // 25
+  cents -= quarters * 25
+  if quarters > 0:
+        print(f"{quarters} quarter(s)")
+    
+  dimes = cents // 10 
+  cents -= dimes * 10
+  if dimes > 0:
+        print(f"{dimes} dime(s)")
+
+  nickels =  cents // 5
+  cents -= nickels * 5
+  if nickels > 0:
+     print(f"{nickels} nickel(s)")
+
+  pennies = cents
+  if pennies > 0:
+     print(f"{pennies} penny(ies)")
+
+def main():
+    cents = get_cents()
+    cal_coins(cents)
+
